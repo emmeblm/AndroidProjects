11519f0af55513ca019e4831c1bd97bb312c7d84
Luisa Emme <lemme@thoughtworks.com>
2015-04-25 19:30:42 -0500

    [Luisa]:[25/04/2015] Creación del Proyecto final medidor de velocidad y nivel con arduino y módulo bluetooth

---
+++ MedidordeNivelyVelocidad/app/src/main/java/com/example/lemme/medidordenivelyvelocidad/MedidorNivel.java
@@ -0,0 +1,14 @@
+package com.example.lemme.medidordenivelyvelocidad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MedidorNivel extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medidor_nivel);
+    }
+}
---
+++ MedidordeNivelyVelocidad/app/src/main/java/com/example/lemme/medidordenivelyvelocidad/MedidorVelocidad.java
@@ -0,0 +1,15 @@
+package com.example.lemme.medidordenivelyvelocidad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MedidorVelocidad extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medidor_velocidad);
+    }
+
+
+}
